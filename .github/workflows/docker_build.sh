2ee5bbf1a4f7a822fa4eac48f5fe912e29a2d82f
thebaptiste <jean-baptiste.veslin@meteo.fr>
2023-08-24 14:32:24 +0000

    build: fix

--- .github/workflows/docker_build.sh
+++ .github/workflows/docker_build.sh
@@ -11,10 +11,7 @@ else
     docker build -t $TAG_BRANCH -t $TAG_LATEST --build-arg BRANCH=$BRANCH --build-arg CACHEBUST=$CACHEBUST .
 fi
 HASH_AFTER=`docker images -q $TAG_BRANCH`
-if [ "${HASH_BEFORE}" == "${HASH_AFTER}" ]; then
-    echo '::set-output name=dispatch_bypass::true'
-else
-    echo '::set-output name=dispatch_bypass::false'
+if [ "${HASH_BEFORE}" != "${HASH_AFTER}" ]; then
     docker push $TAG_BRANCH
     if [ "$BRANCH" == "master" ]; then
         docker push $TAG_LATEST
